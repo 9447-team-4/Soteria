d4a0024950c04b097f8da37ae8c840cf076beaa1
Andy Tang <andytang824@gmail.com>
2021-08-03 14:37:34 +1000

    Auto url entry

--- infrastructure/setupscript/dronecm.sh
+++ infrastructure/setupscript/dronecm.sh
@@ -16,17 +16,17 @@ echo "Setting up drone config map"
 # read GITEA_TOKEN
 echo "Enter your gitea username:"
 read GITEA_USERNAME
-echo "Enter where the gitea server is hosted:"
-read GITEA_HOST
-echo "Enter where the gitea server port:"
-read GITEA_PORT
+# echo "Enter where the gitea server is hosted:"
+# read GITEA_HOST
+# echo "Enter where the gitea server port:"
+# read GITEA_PORT
 echo "Enter gitea repo name:"
 read GITEA_REPO
-echo "Enter where the sonarqube server is hosted:"
-read SONAR_URL
+# echo "Enter where the sonarqube server is hosted:"
+# read SONAR_URL
 echo "Enter your sonarqube username or token:"
 echo "Token can be generated from the page: SonarQube->My Account->Security->Generate Tokens"
-read SONAR_USER
+read SONAR_USERNAME
 echo "Enter your sonarqube password or leave blank if using token:"
 # read -s SONAR_PASSWORD
 stty -echo
@@ -34,12 +34,17 @@ read SONAR_PASSWORD;
 stty echo
 echo "Enter sonarqube project key:"
 read SONAR_KEY
-echo $SONAR_PASSWORD
+# echo $SONAR_PASSWORD
 cd gitea-reporter/
+SONAR_URL=$(minikube service --url sonarqube-sonarqube -n sonarqube)
+GITEA_URL=$(minikube service --url gitea-charts-http -n gitea)
+GITEA_URL=$(echo GITEA_URL | sed "s|//||")
+IFS=':'
+read -a GITEA_URL_ARRAY <<< "$GITEA_URL"
 sed -i "" "s|{{GITEA_TOKEN}}|$GITEA_TOKEN|g" gitea-reporter-configmap.yaml # MACOS
 sed -i "" "s|{{GITEA_USERNAME}}|$GITEA_USERNAME|g" gitea-reporter-configmap.yaml # MACOS
-sed -i "" "s|{{GITEA_HOST}}|$GITEA_HOST|g" gitea-reporter-configmap.yaml # MACOS
-sed -i "" "s|{{GITEA_PORT}}|$GITEA_PORT|g" gitea-reporter-configmap.yaml # MACOS
+sed -i "" "s|{{GITEA_HOST}}|${GITEA_URL_ARRAY[1]}|g" gitea-reporter-configmap.yaml # MACOS
+sed -i "" "s|{{GITEA_PORT}}|${GITEA_URL_ARRAY[2]}|g" gitea-reporter-configmap.yaml # MACOS
 sed -i "" "s|{{GITEA_REPO}}|$GITEA_REPO|g" gitea-reporter-configmap.yaml # MACOS
 sed -i "" "s|{{SONAR_URL}}|$SONAR_URL|g" gitea-reporter-configmap.yaml # MACOS
 sed -i "" "s|{{SONAR_USER}}|$SONAR_USER|g" gitea-reporter-configmap.yaml # MACOS
